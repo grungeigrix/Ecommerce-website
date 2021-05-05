913fcb0e857bf7c7f589f5a3323033c3abf97102
grungeigrix <72521952+grungeigrix@users.noreply.github.com>
2021-05-05 22:04:06 +0300

    Add products

--- ecom/shop/urls.py
+++ ecom/shop/urls.py
@@ -1,8 +1,14 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.home, name= 'home'),
+    path('<slug:category_slug>', views.home, name='products_by_category'),
     path('product/', views.product, name="product"),
-
 ]
+
+if settings.DEBUG:
+	urlpatterns += static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
+	urlpatterns += static(settings.STATIC_URL, document_root= settings.STATIC_ROOT)
