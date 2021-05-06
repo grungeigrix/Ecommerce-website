e75002219c46d9bda9aaf0a6871b749542e5d4a5
grungeigrix <72521952+grungeigrix@users.noreply.github.com>
2021-05-06 14:16:37 +0300

    Add cart

--- ecom/shop/urls.py
+++ ecom/shop/urls.py
@@ -5,8 +5,9 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.home, name= 'home'),
-    path('<slug:category_slug>', views.home, name='products_by_category'),
-    path('product/', views.product, name="product"),
+    path('category/<slug:category_slug>', views.home, name='products_by_category'),
+    path('category/<slug:category_slug>/<slug:product_slug>', views.product, name="product_detail"),
+    path('cart', views.cart, name='cart'),
 ]
 
 if settings.DEBUG:
