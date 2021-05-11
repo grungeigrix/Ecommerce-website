9e17d517eb0ac76b63f52a0cc8c8c2da00f3c839
grungeigrix <72521952+grungeigrix@users.noreply.github.com>
2021-05-11 17:00:42 +0300

    fix cart

--- ecom/shop/views.py
+++ ecom/shop/views.py
@@ -35,11 +35,11 @@ def add_cart(request, product_id):
 		cart = Cart.objects.create(cart_id= _cart_id(request))
 		cart.save()
 	try:
-		cart_item = CartItem.objects.get(product=product, cart= cart)
+		cart_item = CartItem.objects.get(product = product, cart = cart)
 		cart_item.quantity += 1
 		cart_item.save()
 	except CartItem.DoesNotExist:
-		cart_item = Cart.objects.create(product=product, quantity=1, cart=cart)
+		cart_item = CartItem.objects.create(product=product, quantity=1, cart=cart)
 		cart_item.save()
 
 	return redirect ('cart_detail')
